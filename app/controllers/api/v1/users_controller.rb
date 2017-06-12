65ebe3923e1d3c1f6cf4e2e826cb055cdbb75d2f
DenisIzotov <izotov87@gmail.com>
2017-06-12 13:04:30 +1000

    fix users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -41,6 +41,8 @@ class Api::V1::UsersController < Api::V1Controller
         end
     end
 
+private
+
     def user_params
      params.require(:user).permit(  :email,
                                     :name,
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :auth_tokens
   has_many :orders
   has_many :activities
+  has_secure_password
 
   validates :inn,
   :uniqueness => {
